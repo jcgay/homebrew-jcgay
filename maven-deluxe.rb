db24bafaa01222adf71d962027e3bb8e4620d4f3
Jean-Christophe GAY <contact@jeanchristophegay.com>
2015-11-18 22:01:10 +0100

    maven-deluxe 3.3.9-0
    Fixes https://github.com/jcgay/maven-color/issues/10

--- maven-deluxe.rb
+++ maven-deluxe.rb
@@ -1,10 +1,10 @@
 class MavenDeluxe < Formula
   desc "Java-based project management with awesomeness"
   homepage "https://github.com/jcgay/homebrew-jcgay#maven-deluxe"
-  url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz"
-  sha256 "3a8dc4a12ab9f3607a1a2097bbab0150c947ad6719d8f1bb6d5b47d0fb0c4779"
-  version "3.3.3-6"
+  url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
+  sha256 "6e3e9c949ab4695a204f74038717aa7b2689b1be94875899ac1b3fe42800ff82"
+  version "3.3.9-0"
 
   resource 'maven-color-1.2' do
     url 'http://dl.bintray.com/jcgay/maven/com/github/jcgay/maven/color/maven-color-logback/1.2/maven-color-logback-1.2-bundle.tar.gz'
@@ -19,11 +19,6 @@ class MavenDeluxe < Formula
   resource 'maven-profiler-2.4' do
     url 'http://dl.bintray.com/jcgay/maven/fr/jcgay/maven/maven-profiler/2.4/maven-profiler-2.4-shaded.jar'
     sha1 '25e6a2d42bb82a8ebf995ac998e111a9a07a6cec'
-  end
-  
-  resource 'maven-core-patch' do
-    url 'http://jeanchristophegay.com/binaries/maven-core-3.3.3.jar'
-    sha1 'cca6f23c26aeb2cec2f869fc3cc54716c86906f5'
   end
 
   depends_on :java
@@ -53,8 +48,6 @@ class MavenDeluxe < Formula
     inreplace "#{libexec}/conf/logging/logback.xml" do |s|
       s.gsub! "[%replace(%level){'WARN','WARNING'}] ", ""
     end
-    # https://github.com/jcgay/maven-color/issues/10
-    resource("maven-core-patch").stage { (libexec/"lib").install Dir["*"] }
     
     resource("maven-notifier-1.8").stage { (libexec/"lib/ext").install Dir["*"] }
     
@@ -82,6 +75,6 @@ class MavenDeluxe < Formula
         }
       }
     EOS
-    system "#{bin}/mvn", "compile"
+    system "#{bin}/mvn", "compile", "-Duser.home=#{testpath}"
   end
 end
