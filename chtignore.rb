d4aed79335914f36530f4143df9c533d6e6b3446
Jean-Christophe GAY <contact@jeanchristophegay.com>
2016-02-20 19:00:45 +0100

    chtignore 1.1.0

--- chtignore.rb
+++ chtignore.rb
@@ -1,9 +1,9 @@
 class Chtignore < Formula
   desc "Print .gitignore template from https://github.com/github/gitignore in standard output"
   homepage "https://github.com/jcgay/chtignore"
-  url "https://bintray.com/artifact/download/jcgay/tools/chtignore_1.0.0_darwin_amd64.zip"
-  sha256 "87fd82bce0fd177f3d1ae96649bb57b1ec1d134a0b9f58db06306cfac7ac48c6"
-  version "1.0.0"
+  url "https://bintray.com/artifact/download/jcgay/tools/chtignore_1.1.0_darwin_amd64.zip"
+  sha256 "64b6491b3e90c673ca1c1183959f56f6bc8e4d76e39f3fe583431cfb9a42fc69"
+  version "1.1.0"
 
   def install
     bin.install "chtignore"
