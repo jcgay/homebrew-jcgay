e4dfc73230e1621eea11eb9aec82319e4544da5f
Jean-Christophe GAY <contact@jeanchristophegay.com>
2017-01-21 19:58:25 +0100

    Cleanup maven-deluxe.rb using brew audit --strict

--- maven-deluxe.rb
+++ maven-deluxe.rb
@@ -3,25 +3,29 @@ class MavenDeluxe < Formula
   homepage "https://github.com/jcgay/homebrew-jcgay#maven-deluxe"
   url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
   mirror "https://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
-  sha256 "6e3e9c949ab4695a204f74038717aa7b2689b1be94875899ac1b3fe42800ff82"
   version "3.3.9-6"
+  sha256 "6e3e9c949ab4695a204f74038717aa7b2689b1be94875899ac1b3fe42800ff82"
 
-  resource 'maven-color-1.6.0' do
-    url 'http://dl.bintray.com/jcgay/maven/com/github/jcgay/maven/color/maven-color-logback/1.6.0/maven-color-logback-1.6.0-bundle.tar.gz'
-    sha256 'c6f109712061c55608db0aa45abfe127faf7ea08ca19dd04adae5be2247afc44'
-  end
-  
-  resource 'maven-notifier-1.9.1' do
-    url 'http://dl.bintray.com/jcgay/maven/fr/jcgay/maven/maven-notifier/1.9.1/maven-notifier-1.9.1-shaded.jar'
-    sha256 'ed6fbb0bffc633cf43b4f52d8aae33ac1ce313f7528ca4aecaa75559f8a3bfd5'
-  end
-  
-  resource 'maven-profiler-2.4' do
-    url 'http://dl.bintray.com/jcgay/maven/fr/jcgay/maven/maven-profiler/2.4/maven-profiler-2.4-shaded.jar'
-    sha256 '2ede9a5f5646ba853dd1926142b0f69fdfabdb08926bf403746fcd4f7f433eed'
-  end
+  bottle :unneeded
 
   depends_on :java
+
+  conflicts_with "mvnvm", :because => "also installs a 'mvn' executable"
+
+  resource "maven-color-1.6.0" do
+    url "https://dl.bintray.com/jcgay/maven/com/github/jcgay/maven/color/maven-color-logback/1.6.0/maven-color-logback-1.6.0-bundle.tar.gz"
+    sha256 "c6f109712061c55608db0aa45abfe127faf7ea08ca19dd04adae5be2247afc44"
+  end
+
+  resource "maven-notifier-1.9.1" do
+    url "https://dl.bintray.com/jcgay/maven/fr/jcgay/maven/maven-notifier/1.9.1/maven-notifier-1.9.1-shaded.jar"
+    sha256 "ed6fbb0bffc633cf43b4f52d8aae33ac1ce313f7528ca4aecaa75559f8a3bfd5"
+  end
+
+  resource "maven-profiler-2.4" do
+    url "https://dl.bintray.com/jcgay/maven/fr/jcgay/maven/maven-profiler/2.4/maven-profiler-2.4-shaded.jar"
+    sha256 "2ede9a5f5646ba853dd1926142b0f69fdfabdb08926bf403746fcd4f7f433eed"
+  end
 
   def install
     # Remove windows files
@@ -41,16 +45,14 @@ class MavenDeluxe < Formula
       next if basename.to_s == "m2.conf"
       (bin/basename).write_env_script file, Language::Java.overridable_java_home_env
     end
-    
+
     # Remove slf4j-simple
     rm_f Dir[libexec/"lib/slf4j-simple*"]
 
-    resource("maven-color-1.6.0").stage { system "cp", "-r", ".", libexec }    
+    resource("maven-color-1.6.0").stage { system "cp", "-r", ".", libexec }
     resource("maven-notifier-1.9.1").stage { (libexec/"lib/ext").install Dir["*"] }
-    resource("maven-profiler-2.4").stage { (libexec/"lib/ext").install Dir["*"] }    
+    resource("maven-profiler-2.4").stage { (libexec/"lib/ext").install Dir["*"] }
   end
-
-  conflicts_with "mvnvm", :because => "also installs a 'mvn' executable"
 
   test do
     (testpath/"pom.xml").write <<-EOS.undent
