71ff718f5311bef8c951fa2947f0097aa122afe8
Jean-Christophe GAY <contact@jeanchristophegay.com>
2017-04-07 23:38:08 +0200

    maven-deluxe 3.5.0-0 with Maven 3.5.0

--- maven-deluxe.rb
+++ maven-deluxe.rb
@@ -1,10 +1,10 @@
 class MavenDeluxe < Formula
   desc "Java-based project management with awesomeness"
   homepage "https://github.com/jcgay/homebrew-jcgay#maven-deluxe"
-  url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
-  version "3.3.9-8"
-  sha256 "6e3e9c949ab4695a204f74038717aa7b2689b1be94875899ac1b3fe42800ff82"
+  url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz"
+  version "3.5.0-0"
+  sha256 "beb91419245395bd69a4a6edad5ca3ec1a8b64e41457672dc687c173a495f034"
 
   bottle :unneeded
 
@@ -47,7 +47,7 @@ class MavenDeluxe < Formula
     end
 
     # Remove slf4j-simple
-    rm_f Dir[libexec/"lib/slf4j-simple*"]
+    rm_f Dir[libexec/"lib/maven-slf4j-provider*"]
 
     resource("maven-color-2.0.0-beta").stage { (libexec/"lib/ext").install Dir["ext/*"] }
     resource("maven-notifier-1.9.1").stage { (libexec/"lib/ext").install Dir["*"] }
