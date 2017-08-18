7d2add9a3f221e2fe65943131ffa15e1d166a252
Jean-Christophe GAY <contact@jeanchristophegay.com>
2017-08-18 12:32:07 +0200

    parallel-git-repo 1.0.1

--- parallel-git-repo.rb
+++ parallel-git-repo.rb
@@ -1,11 +1,15 @@
 class ParallelGitRepo < Formula
   desc "Run command on git repositories in parallel"
   homepage "https://github.com/jcgay/parallel-git-repo"
-  url "https://bintray.com/jcgay/tools/download_file?file_path=parallel-git-repo-darwin-amd64", :using => :nounzip
-  version "1.0.0"
-  sha256 "2c41419e8d4617abc5ea342e2b35a756a22bb320be5603130b1225b2079053b7"
+  url "https://bintray.com/jcgay/tools/download_file?file_path=v1.0.1%2Fparallel-git-repo-darwin-amd64", :using => :nounzip
+  version "1.0.1"
+  sha256 "ced09d3c4c62f104116703db25f7278cd6756caed939beb4778df2083129f0e0"
 
   def install
     bin.install Dir["*"].shift => "parallel-git-repo"
+  end
+  
+  test do
+    system "#{bin}/parallel-git-repo", "-h"
   end
 end
