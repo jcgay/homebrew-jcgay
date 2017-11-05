499659517a1dad0587b0333d95214fd18f044627
Jean-Christophe GAY <contact@jeanchristophegay.com>
2017-11-05 20:32:11 +0100

    send-notification 0.14.0

--- send-notification.rb
+++ send-notification.rb
@@ -1,9 +1,9 @@
 class SendNotification < Formula
   desc "Command Line to send notifications"
   homepage "https://github.com/jcgay/send-notification"
-  url "https://bintray.com/artifact/download/jcgay/maven/fr/jcgay/send-notification/send-notification-cli/0.13.0/send-notification-cli-0.13.0-binaries.zip"
-  version "0.13.0"
-  sha256 "00b4b106d88f3cf0c405fbcf414cd852a156b0ec4e5a2f33fadd1d6f9340bb4f"
+  url "https://bintray.com/artifact/download/jcgay/maven/fr/jcgay/send-notification/send-notification-cli/0.14.0/send-notification-cli-0.14.0-binaries.zip"
+  version "0.14.0"
+  sha256 "11a559cf45c1b3ecde8fb2b5ddd26abae8cb4ee682652c423a3b7ccd2e56eafc"
   
   def install
     # Remove windows files
