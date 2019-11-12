0dd6a053a466f3993e2ecfcfc81937d84ffd5fe8
Jean-Christophe Gay <contact@jeanchristophegay.com>
2019-11-12 19:56:26 +0100

    gw 0.2

--- gw.rb
+++ gw.rb
@@ -1,9 +1,9 @@
 class Gw < Formula
     desc "Gradle Wrapper with interactive cli"
     homepage "https://github.com/jcgay/gw"
-    url "https://github.com/jcgay/gw/releases/download/v0.1/gw-refs.tags.v0.1.zip"
-    version "0.1"
-    sha256 "11355745b7aaafa8dcaa0a3c5516f582eadebdd188f9e1812b10864e481cbe32"
+    url "https://github.com/jcgay/gw/releases/download/v0.2/gw-refs.tags.v0.2.zip"
+    version "0.2"
+    sha256 "088b4b3ba18dcb0888280c33a34a067ef76f25484b7fcd8c9b1ff9cdcd72b422"
     
     def install
       # Remove windows files
